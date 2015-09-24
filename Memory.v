b9dd66b2967498b2d48bf4359b93e21fe731e930
d-h-b <daniel.holod.betz@gmail.com>
2015-09-24 16:57:33 -0600

    Update Memory.v

--- Memory.v
+++ Memory.v
@@ -19,22 +19,59 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Memory(
-    input [14:0] Port_A_Address,
-    input [14:0] Port_B_Address,
-    input [15:0] Port_A_In,
-    input [15:0] Port_B_In,
-    input Port_A_WE,
-    input Port_B_WE,
-    input Clk,
-	 output [15:0] Port_A_Out,
-    output [15:0] Port_B_Out
+    input [14:0] port_a_address,
+    input [14:0] port_b_address,
+    input [15:0] port_a_in,
+    input [15:0] port_b_in,
+	 
+    input port_a_we,
+    input port_b_we,
+    input clk,
+	 output [15:0] port_a_out,
+    output [15:0] port_b_out
     );
 
-	//initial
-	//$readmemh("<data_file_name>", <rom_name>, <begin_address>, <end_address>);
-
+	parameter RAM_WIDTH = 16; //<ram_width>;
+	parameter NUM_RAM_ADDR_BITS = (1024*32)-1; //<ram_addr_bits>;
+	parameter ADDRESS_WIDTH = 15;
+	
+	//logical/clock inputs
+    wire port_a_we;
+    wire port_b_we;
+    wire clk;
+	 
+	 // Port A
+	 wire [RAM_WIDTH-1:0] port_a_in;
+	 wire [14:0] port_a_address;
+	 reg [RAM_WIDTH-1:0] port_a_out;
+	 
+	 //Port B
+	 wire [RAM_WIDTH-1:0] port_b_in;
+	 wire [ADDRESS_WIDTH-1:0] port_b_address;
+	 reg [RAM_WIDTH-1:0] port_b_out;
+	 
+	reg [RAM_WIDTH-1:0] the_memory_core [RAM_ADDR_BITS:0];
+	
    always @(posedge clk)
 	begin
-		
+	
+         if (port_a_we == 1'b1) 
+			begin
+            the_memory_core[port_a_address] <= port_a_in;
+				// In write-first mode, the din is also passed on to dout
+            port_a_out <= port_a_in;
+         end
+         else
+            port_a_out <= the_memory_core[port_a_address];
+     
+         if (port_b_we == 1'b1) 
+			begin
+            the_memory_core[port_b_address] <= port_b_in;
+				// In write-first mode, the din is also passed on to dout
+            port_b_out <= port_b_in;
+         end
+         else
+            port_b_out <= the_memory_core[port_b_address];
+     
 	end
 endmodule
