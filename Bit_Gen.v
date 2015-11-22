a84dc29d334173bc867dfef7ebb7d5eb63af8402
Billzabob <cocolymoo@gmail.com>
2015-11-21 19:20:43 -0700

    Drawing zeros on screen

--- Bit_Gen.v
+++ Bit_Gen.v
@@ -28,22 +28,59 @@ module Bit_Gen(
     output reg [7:0] green,
     output reg [7:0] blue
     );
-	 
-	 reg set = 0;
-	
+
 	always@(NineMHz)
 	begin
-		set <= ~set;
-		if(set == 1'b1)
+		if(NineMHz == 1'b0)
 		begin
-			port_b_address <= port_b_out + 720;		//Set address to glyph data
+			port_b_address = (port_b_out * 128) + 1021 + h_count[2:0] + (v_count[3:0] * 8);		//Set address to glyph data
 		end
 		else
 		begin
-			red <= port_b_out[15:11];
-			green <= port_b_out[10:6];
-			blue <= port_b_out[5:0];
-			port_b_address <= h_count[9:3] + (v_count[8:4] * 60); //Set address to 0-720 for each glyph
+			red = {port_b_out[15:11],3'b1};
+			green = {port_b_out[10:6],3'b1};
+			blue = {port_b_out[5:0],2'b1};
+			port_b_address = h_count[9:3] + (v_count[8:4] * 60); //Set address to 0-1020 for each glyph
 		end
 	end
+	/*
+	initial
+	begin
+		port_b_address = 0;
+	end
+	
+	always@(*)
+	begin
+		if((h_count[9:3] + (v_count[8:4] * 60)) >= 510)
+		begin
+			if((h_count[2:0] + (v_count[3:0] * 8)) >= 64)
+			begin
+				red = 8'b11111111;
+				green = 8'b00000000;
+				blue = 8'b00000000;
+			end
+			else
+			begin
+				red = 8'b11111111;
+				green = 8'b11111111;
+				blue = 8'b00000000;
+			end
+		end
+		else
+		begin
+			if((h_count[2:0] + (v_count[3:0] * 8)) >= 64)
+			begin
+				red = 8'b11111111;
+				green = 8'b00000000;
+				blue = 8'b11111111;
+			end
+			else
+			begin
+				red = 8'b00000000;
+				green = 8'b11111111;
+				blue = 8'b00000000;
+			end
+		end
+	end
+	*/
 endmodule
